7c2c63688d550c493a4e0e0c5710e390c1522f1f
Benjamin Muñoz <benjamin.munoz.tapia@gmail.com>
2020-05-09 22:30:41 -0400

    parte 1 lista

--- Lab1/lab1.m
+++ Lab1/lab1.m
@@ -7,29 +7,55 @@
 %a(x) = 8log5(4x+12) (en rojo con *) intervalo [0,15pi] espacio de 0.01
 x1 = 0:0.01:15*pi;
 a = 8*(log(4*x1+12)/log(5)); %se aplica la propiedad de logaritmo para obtener log5(x)
+
+%Grafico de la primera funcion
+figure
+plot(x1,a,'r *');
 title('Función a(x) = 8log5(4x+12)');
-ylabel('Eje y');
-xlabel('Eje x');
+ylabel('y');
+xlabel('x');
 %plot(x1,a,'r *');
 
 
 %b(x) = sen(6(log2(x+9))) + cos(7(log6(4x+32))) (en verde con +)
 x2 = 0:0.01:15*pi;
 b = sin(6*(log2(x2+9))) + cos(7*(log(4*x2+32)/log(6)));
-title ('Función b(x) = sen(6(log2(x+9))) + cos(7(log6(4x+32)))');
-ylabel('Eje y');
-xlabel('Eje x');
-%plot(x2,b,'g +');
 
-%Grafico de la primera funcion
-figure(1);
-plot(x1,a,'r *');
 %Grafico de la segunda funcion
-figure(2);
+figure
 plot(x2,b,'g +');
+title ('Función b(x) = sen(6(log2(x+9))) + cos(7(log6(4x+32)))');
+ylabel('y');
+xlabel('x');
+
+
+
+
 %Ambos juntos
-figure(3);
+figure
+hold on;
 plot(x1,a,'r *');
-hold on
+
 plot(x2,b,'g +');
-hold off
+
+hold off;
+
+
+
+%parte 2
+%Escala normal
+x3 = -10:0.05:10;
+c = 6 * exp(x3+18);
+figure
+plot(x3,c,'g+');
+title('Función 6*exp(x+18) Escala normal');
+ylabel('y');
+xlabel('x');
+grid on;
+%Escala logaritmica
+figure
+semilogy(x3,c,'r-');
+title('Función 6*exp(x+18) Escala Logarítmica');
+ylabel('y');
+xlabel('x');
+grid on;
