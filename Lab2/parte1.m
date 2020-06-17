dbefddf954d2b0ed5062729e68a1677d4102464a
maximilianoarevalo <maximiliano.arevalo@usach.cl>
2020-06-16 20:34:53 -0400

    Parte 1 Lista

--- Lab2/parte1.m
+++ Lab2/parte1.m
@@ -14,3 +14,11 @@ sys2 = tf(num2,den2);
 step(sys2);
 [num2_2,den2_2] = cloop(num2,den2);
 step(num2_2,den2_2);
+
+%funcion 3
+num3 = [0,0,12,0];
+den3 = [1,7,0,-4];
+sys3 = tf(num3,den3);
+step(sys3);
+[num3_2,den3_2] = cloop(num3,den3);
+step(num3_2,den3_2);
