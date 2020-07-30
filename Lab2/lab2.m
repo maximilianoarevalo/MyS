006b61a4b32b1fea3d411ac60e52d128347852c3
Benjamín <benjamin.munoz.tapia@gmail.com>
2020-07-29 21:45:25 -0400

    se agrega parte 2

--- Lab2/lab2.m
+++ Lab2/lab2.m
@@ -1,31 +1,15 @@
-%Funcion 1
-num1 = [0 0 4 0];
-den1 = [0 10 0 4];
+H1 = tf(4,[10,0,4]);
+H2 = tf(3,[6,16]);
+H3 = tf([4,10],[4,9,5,0]);
+H4 = tf(1,[7,10]);
+H5 = tf([8,8],[1,2,3,0]);
+H6 = tf([3,2],[5,7,10]);
 
-%Funcion 2
-num2 = [0 0 0 3];
-den2 = [0 0 6 16];
+A1 = feedback(H3,-1)
+A2 = parallel(H4,H5)
 
-%Funcion 3
-num3 = [0 0 4 10];
-den3 = [4 9 5 0];
+A = A1*A2*H6
 
-%Funcion 4
-num4 = [0 0 0 1];
-den4 = [0 0 7 10];
+final = H1+H2+A
 
-%Funcion 5
-num5 = [0 0 8 8];
-den5 = [1 2 3 0];
-
-%Funcion 6
-num6 = [0 0 3 2];
-den6 = [0 5 7 10];
-
-[n1, d1] = parallel(num1,den1,num2,den2);
-[n2, d2] = feedback(num3,den3);
-[n3, d3] = parallel(num4,den4,num5,den4);
-[n4, d4] = series(n3,d3,num6,den6);
-[n5, d5] = parallel(num2,den2,num3,den3);
-
-
+step(final)
