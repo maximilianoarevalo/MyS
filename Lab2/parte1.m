97c1abb89002ef0a73669c398425d7fd829c4dc1
maximilianoarevalo <maximiliano.arevalo@usach.cl>
2020-07-30 21:38:09 -0400

    Graficos

--- Lab2/parte1.m
+++ Lab2/parte1.m
@@ -8,9 +8,13 @@ den1 = [3,1];
 sys1 = tf(num1,den1);
 %Grafico step lazo abierto
 step(sys1);
+%title("Función de transferencia 1: Lazo Abierto");
+%grid on
 %Grafico step lazo cerrado utilizando cloop
 [num1_2,den1_2] = cloop (num1,den1);
 step(num1_2,den1_2);
+%title("Función de transferencia 1: Lazo Cerrado");
+%grid on
 
 %Funcion 2
 %Definicion de numerador y denominador funcion de transferencia
@@ -20,9 +24,13 @@ den2 =[1,6,3] ;
 sys2 = tf(num2,den2);
 %Grafico step lazo abierto
 step(sys2);
+%title("Función de transferencia 2: Lazo Abierto");
+%grid on
 %Grafico step lazo cerrado utilizando cloop
 [num2_2,den2_2] = cloop(num2,den2);
 step(num2_2,den2_2);
+%title("Función de transferencia 2: Lazo Cerrado");
+%grid on
 
 %Funcion 3
 %Definicion de numerador y denominador funcion de transferencia
@@ -32,6 +40,10 @@ den3 = [1,7,0,-4];
 sys3 = tf(num3,den3);
 %Grafico step lazo abierto
 step(sys3);
+%title("Función de transferencia 3: Lazo Abierto");
+%grid on
 %Grafico step lazo cerrado utilizando cloop
 [num3_2,den3_2] = cloop(num3,den3);
 step(num3_2,den3_2);
+%title("Función de transferencia 3: Lazo Cerrado");
+%grid on
